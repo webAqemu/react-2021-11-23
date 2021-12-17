e45a74e55d89143a681f4d070315047227e89830
webAqemu <notych2812@gmail.com>
2021-12-17 10:41:27 +0300

    some changes

--- src/components/restaurant/restaurant.js
+++ src/components/restaurant/restaurant.js
@@ -1,13 +1,17 @@
 import { useMemo, useState } from 'react';
+import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import Menu from '../menu';
 import Reviews from '../reviews';
 import Banner from '../banner';
 import Rate from '../rate';
 import Tabs from '../tabs';
+import reviews from '../../redux/reducer/reviews';
 
 const Restaurant = ({ restaurant }) => {
   const { id, name, menu, reviews } = restaurant;
+
+  console.log(reviews);
 
   const [activeTab, setActiveTab] = useState('menu');
 
@@ -46,4 +50,16 @@ Restaurant.propTypes = {
   }).isRequired,
 };
 
-export default Restaurant;
+const mapStateToProps = (state, props) => ({
+  reviews: props.restaurant.reviews.map((id) => {
+    console.log(props.restaurant.reviews);
+    return state.reviews[id];
+  }),
+});
+
+export default connect((state, props) => {
+  console.log(state, props);
+  return {
+    reviews: reviews(state, props.reviews),
+  };
+})(Restaurant);
--- src/redux/selectors.js
+++ src/redux/selectors.js
@@ -3,6 +3,7 @@ import { createSelector } from 'reselect';
 // const restaurantsSelector = (state) => state.restaurants;
 const productsSelector = (state) => state.products;
 const orderSelector = (state) => state.order;
+const reviewsSelector = (state) => state.reviews;
 
 export const orderProductsSelector = createSelector(
   [productsSelector, orderSelector],
@@ -21,4 +22,10 @@ export const totalSelector = createSelector(
   [orderProductsSelector],
   (orderProducts) =>
     orderProducts.reduce((acc, { subtotal }) => acc + subtotal, 0)
+);
+
+export const reviews = createSelector([reviewsSelector], (state, reviewsId) =>
+  reviewsId.map((id) => {
+    return reviewsSelector(state)[id];
+  })
 );
